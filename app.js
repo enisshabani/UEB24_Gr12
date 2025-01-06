0ed630427c43fc57ca66bfa83b7c743dcd772e2a
Enis Shabani <shabanienis44@gmail.com>
2025-01-06 22:09:35 +0100

    Scroll Reveal

--- app.js
+++ app.js
@@ -15,3 +15,11 @@ const sr = ScrollReveal({
     delay: 400,
     reset: true
 })
+
+sr.reveal('.text',{delay: 200, origin: 'top'})
+sr.reveal('.form-container form',{delay: 800, origin: 'left'})
+sr.reveal('.heading',{delay: 500, origin: 'top'})
+sr.reveal('.ride-container .box',{delay: 600, origin: 'top'})
+sr.reveal('.services-container .box',{delay: 400, origin: 'top'})
+sr.reveal('.about-container .box',{delay: 600, origin: 'top'})
+sr.reveal('.reviews-container',{delay: 600, origin: 'top'})
