606ee4479b815123e005defbff13fdc2e0dd6b11
RinesaS1 <rinesaselmonaj4@gmail.com>
2025-01-07 09:01:05 +1300

    Improved car specifications table

---
+++ app.js
@@ -0,0 +1,12 @@
+const cars = [
+    {
+        name: "AUDI",
+        make: "AUDI",
+        model: "Class S",
+        year: 2014,
+        mileage: "104,556",
+        version: "1.6 hdi",
+        fuel: "Diesel",
+        engine: "1900",
+        horsepower: 200,
+    },
