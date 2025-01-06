b22d72e9fd03367f9a0e7eabe1b58a7b167f6bf1
Enis Shabani <shabanienis44@gmail.com>
2025-01-06 22:48:13 +0100

    Countdown

--- app.js
+++ app.js
@@ -211,4 +211,11 @@ const countdownEndDate = new Date("2025-01-10T00:00:00").getTime();
 function updateCountdown() {
   const now = new Date().getTime();
   const timeLeft = countdownEndDate - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+
 }
