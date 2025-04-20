9d252a1a4982f58dbc129844d2f28f69d7051ce9
ShqiperiMjeku1 <shqiperimjeku1@gmail.com>
2025-04-20 20:47:35 +0200

    Update sign-in.php

--- sign-in.php
+++ sign-in.php
@@ -12,4 +12,17 @@ class User {
     public function getUsername() {
         return $this->username;
     }
+    
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
 }
+
