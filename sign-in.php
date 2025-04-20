c1c542832c7f1c4ae5fc545ca4de507dece9f69e
ShqiperiMjeku1 <shqiperimjeku1@gmail.com>
2025-04-20 20:46:12 +0200

    Update sign-in.php

--- sign-in.php
+++ sign-in.php
@@ -1,1 +1,10 @@
+<?php
+class User {
+    private $username;
+    private $password;
 
+    public function __construct($username, $password) {
+        $this->username = $username;
+        $this->password = $password;
+    }
+}
